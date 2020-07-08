ab337479dafe9939bf32151792fabc27b85b9729
defqx <defqx@github.com>
2020-07-08 16:13:08 +0800

    添加Neo4j节点 添加关系

--- src/api/metamodel/neonode.js
+++ src/api/metamodel/neonode.js
@@ -1,10 +1,19 @@
 import request from '@/router/axios';
 
 export const add = (row) => {
-  console.log(row)
   return request({
     url: '/api/blade-neo4j/addNode',
     method: 'post',
     data: row
   })
 }
+
+export const addRelation = (row) => {
+  console.log(row)
+  return request({
+    url: '/api/blade-neo4j/addRelation',
+    method: 'post',
+    data: row
+  })
+}
+
--- src/views/metamodel/neoedge.vue
+++ src/views/metamodel/neoedge.vue
@@ -10,7 +10,7 @@
 
 <script>
   import {
-    add
+    addRelation
   } from "@/api/metamodel/neonode";
 
   export default {
@@ -22,6 +22,22 @@
         option: {
           labelWidth: 110,
           column: [{
+            label: '关系开始节点',
+            prop: "startNode",
+            type: "select",
+            dicUrl: "/api/blade-neo4j/select",
+            span:12,
+            row: true
+          },
+          {
+            label: '关系结束节点',
+            prop: "endNode",
+            type: "select",
+            dicUrl: "/api/blade-neo4j/select",
+            span:12,
+            row: true
+          },
+          {
             label: '标签',
             prop: "label",
             span:12,
@@ -60,16 +76,16 @@
         }
       }
     },
-
     methods:{
-      handleSubmit(form, done){
-        add(this.obj).then(() => {
+      handleSubmit(form,done){
+        console.log(this.obj);
+        console.log();
+        addRelation(this.obj).then(() => {
           done();
           this.$message({
             type: "success",
             message: "操作成功!"
           });
-
         });
         // this.$message.success(JSON.stringify(this.obj))
       }
